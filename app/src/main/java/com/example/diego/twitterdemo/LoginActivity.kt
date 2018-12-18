0b14b91bfe1f54107cfdc57c74d7be6ce27f48d5
diegitsen <diegitsen@gmail.com>
2018-12-18 04:10:49 -0500

    databse, auth and storage done

--- app/src/main/java/com/example/diego/twitterdemo/LoginActivity.kt
+++ app/src/main/java/com/example/diego/twitterdemo/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.example.diego.twitterdemo
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -11,22 +13,130 @@ import android.provider.MediaStore
 import android.support.v4.app.ActivityCompat
 import android.view.View
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_login.*
+import java.io.ByteArrayOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.math.PI
 
 class LoginActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth?=null
+
+    private var database=FirebaseDatabase.getInstance()
+    private var myRef = database.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        /*
-        ivPicture.setOnClickListener(View.OnClickListener {
-            //TODO: select image from the phone
-                checkPermission()
-        })*/
+        mAuth = FirebaseAuth.getInstance()
+
 
         ivPicture.setOnClickListener { checkPermission() }
+    }
+
+    fun loginWithFirebase(email:String, password:String)
+    {
+        mAuth!!.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){ //notifica si la operacion fue un exito o una fail
+                    task ->
+                if(task.isSuccessful)
+                {
+                    Toast.makeText(this, "Succedfull login", Toast.LENGTH_SHORT).show()
+                    saveImageInFirebase()
+
+
+                }
+                else
+                {
+                    Toast.makeText(this, "Fail login", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    fun loadTweets()
+    {
+        var currentUser = mAuth!!.currentUser
+
+        if(currentUser!=null)
+        {
+            var intent = Intent(this, MainActivity::class.java)
+
+            intent.putExtra("email", currentUser.email)
+            intent.putExtra("uid", currentUser.uid)
+
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadTweets()
+    }
+
+
+    fun saveImageInFirebase()
+    {
+        var currentUser = mAuth!!.currentUser
+        val storage = FirebaseStorage.getInstance()
+        val storageRef = storage.getReferenceFromUrl("gs://twitterdemo-8db3f.appspot.com")
+        val df = SimpleDateFormat("ddMMyyHHmmss")
+        val dataobj = Date()
+        val email:String = currentUser!!.email.toString()
+        val imagePath = splitString(email) + "." + df.format(dataobj) + ".jpg"
+        //esto guarda en la subcarpeta images, la ruta , por asi decrilo, de la imagen subida
+        val imageRef = storageRef.child("images/" + imagePath)
+        ivPicture.isDrawingCacheEnabled=true
+        ivPicture.buildDrawingCache()
+
+        //proceso para convertir imagen a bitmap
+        val drawable = ivPicture.drawable as BitmapDrawable
+        val bitmap = drawable.bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+        val uploadTask=imageRef.putBytes(data)
+        uploadTask.addOnFailureListener()//////////////
+        {
+            Toast.makeText(applicationContext, "fallo al subir", Toast.LENGTH_SHORT).show()
+        }.addOnSuccessListener {
+
+                taskSnapshot ->var downloadURL = taskSnapshot.metadata!!.reference!!.downloadUrl!!.toString()
+
+                Toast.makeText(this, "yeees", Toast.LENGTH_SHORT).show()
+                myRef.child("Users").child(currentUser.uid).child("email").setValue(currentUser.email)
+                myRef.child("Users").child(currentUser.uid).child("Profile Image").setValue(downloadURL)
+                loadTweets()
+        }
+
+
+
+
+
+
+
+
+/*
+        uploadTask.addOnFailureListener()//////////////
+        {
+            Toast.makeText(applicationContext, "fallo al subir", Toast.LENGTH_SHORT).show()
+        }.addOnSuccessListener {
+               taskSnapshot-> var DownloadURL = taskSnapshot
+
+        }
+*/
+
+    }
+
+    fun splitString(str:String):String
+    {
+        var split = str.split("@")
+        return split[0]
     }
 
     val READIMAGE = 123
@@ -34,6 +144,9 @@ class LoginActivity : AppCompatActivity() {
     {
         if(Build.VERSION.SDK_INT >= 23)
         {
+            //activityCompat -> ayudante que te facilita acceder a las funciones del activity
+            //(context, permission)
+            //packageManager->recupera los tipos de informacion de los paquetes instalados
             if(ActivityCompat.checkSelfPermission(this,
                     android.Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED)
             {
@@ -42,6 +155,7 @@ class LoginActivity : AppCompatActivity() {
             }
 
             loadImage()
+
         }
 
 
@@ -78,6 +192,7 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
+    //contentResolver -> permite manipular los datos de la memoria
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -98,7 +213,6 @@ class LoginActivity : AppCompatActivity() {
 
     fun bLogin(view: View)
     {
-        var intent=Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        loginWithFirebase(etEmail.text.toString(), etPassword.text.toString())
     }
 }
